d8ac636acd8a2ca19e9854c438a465f475b745bb
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-01-10 10:18:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
 st.title("Assignments")
-st.selectbox("Page", ["Add", "View"])
+page = st.selectbox("Page", ["Add", "View"])
+Assignments = []
+
+if page == "Add":
+  x = st.text_input("Assignment")
+  if st.button("Add"):
+    Assignments.append(x)
+
+for g in Assignments:
+  st.text(g)
