1a853a29a10bf5e32d46b77a480dcefafbbb9cd5
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-01-10 10:20:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ st.title("Assignments")
 page = st.selectbox("Page", ["Add", "View"])
 Assignments = []
 
-if page == "Add":
-  x = st.text_input("Assignment")
-  if st.button("Add"):
-    Assignments.append(x)
+while true:
+  if page == "Add":
+    x = st.text_input("Assignment")
+    if st.button("Add"):
+      Assignments.append(x)
 
 for g in Assignments:
   st.text(g)
