5894e36f97b767469086fa32754f5ce71e2c7e9c
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2024-01-11 08:22:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import streamlit as st
+from replit import db
 st.title("Assignments")
 page = st.selectbox("Page", ["Add", "View"])
 
+db["list"] = []
 
 
 
 if page == "Add":
   x = st.text_input("Assignment")
   if st.button("Add"):
-    list.append(x)
+    db["list'].append(x)
 
-for g in list:
+for g in db["list"]:
   st.text(g)
